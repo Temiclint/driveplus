0d845f9565bfa4bbbf74441676f35886ea815cb1
temiclint <direadelaja@gmail.com>
2024-05-23 20:41:52 +0100

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => (
   <div className={`$[styles.flexStart] md:flex-row flex-col mb-8 w-full`}>
     <div className="flex-1 flex flex-col justify-start mr-10">
       <img src={logo} alt="hoobank" className ="w-[266px] h-[72px] object-contain"/>
-      <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>A new way to make the payments easy, reliable and secure.</p>
+      <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>The Next Generation Car Pooling service.</p>
     </div>
     <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
       {footerLinks.map((footerLink) =>(
@@ -28,7 +28,7 @@ const Footer = () => (
   <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 
   border-t-[1px] border-t-[#3f3e45]">
     <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-gray-700">Copyright
-2021 HooBank. All Rights Reserved.</p>
+2022 Drive Plus. All Rights Reserved.</p>
 <div className="flex flex-row md:mt-0 mt-6">
 {socialMedia.map((social, index)=>(
   <img key={social.id} src={social.icon} alt={social.id} className={`w-[21px] h-[21px]
